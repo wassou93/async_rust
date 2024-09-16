97e6d35c03ef152375a017ba40e0eba5339d6dbe
Wassim Akkari <was.akkari@gmail.com>
2024-09-16 12:34:16 +0100

    Forcing task to yield and move to the end of the executor queue to give chance to other tasks to run cooperatively.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-
 async fn ticker() {
     for i in 0..10 {
         println!("tick {i}");
+        tokio::task::yield_now().await;
     }
 }
 
 async fn tocker() {
     for i in 0..10 {
         println!("tock {i}");
+        tokio::task::yield_now().await;
     }
 }
 
 #[tokio::main]
 async fn main() {
-    tokio::spawn(ticker());
-    tokio::spawn(tocker());
+    let _ = tokio::join!(
+        tokio::spawn(ticker()), 
+        tokio::spawn(tocker()),
+    );
     println!("Main Task!");
 }
