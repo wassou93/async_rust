8a7fd4d158ebcf96acdbec4e555f943dfe0afc28
Wassim Akkari <was.akkari@gmail.com>
2024-09-16 12:22:49 +0100

    Run tasks that doesn't yield

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,20 @@
-use tokio::task::JoinSet;
 
+async fn ticker() {
+    for i in 0..10 {
+        println!("tick {i}");
+    }
+}
+
+async fn tocker() {
+    for i in 0..10 {
+        println!("tock {i}");
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    println!("Hello from async!");
-
-    // Using tokio macro
-    let (double_two, double_five) = tokio::join!(double(2), double(5));
-    println!("Double of 2: {}", double_two);
-    println!("Double of 5: {}", double_five);
-
-    // Using futures 
-    let futures = vec![double(2), double(5)];
-    let result = futures::future::join_all(futures).await;
-    println!("{:?}", result);
-
-    // Using tokio JoinSet - unordered result and faster
-    let mut set = JoinSet::new();
-    for i in 0..10 {
-        set.spawn(double(i));
-    }
-
-    while let Some(res) = set.join_next().await {
-        println!("{res:?}");
-    }
-
-}
-
-async fn double(number: i32) -> i32 {
-    number * 2
+    let _ = tokio::join!(
+        tokio::spawn(ticker()),
+        tokio::spawn(tocker()),
+    );
 }
