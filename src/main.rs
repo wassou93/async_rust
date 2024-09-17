df740dbd82703f4ffc6129884c35dce4b99ff9d5
Wassim Akkari <was.akkari@gmail.com>
2024-09-17 07:34:54 +0100

    mpsc example with 2 senders and one receiver, senders have underlying thread safety so you can clone it and send it to another async task without additional synchronization primitives

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,33 @@
-fn is_prime(n: u32) -> bool {
-    (2..n/2).all(|i| n % i != 0)
+enum Message {
+    Tick(i32),
 }
 
-async fn slow_counter() -> usize {
-    tokio::task::spawn_blocking(move || {
-        (2 .. 100_000).filter(|&x| is_prime(x)).count()
-    }).await.unwrap()
-}
-
-async fn ticker() {
+async fn sender1(tx: tokio::sync::mpsc::Sender<Message>) {
     loop {
-        println!("Still alive!");
+        tx.send(Message::Tick(1)).await.unwrap();
         tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    }
+}
+
+async fn sender2(tx: tokio::sync::mpsc::Sender<Message>) {
+    loop {
+        tx.send(Message::Tick(2)).await.unwrap();
+        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    }
+}
+
+async fn receiver(mut rx: tokio::sync::mpsc::Receiver<Message>) {
+    while let Some(message) = rx.recv().await {
+        match message {
+            Message::Tick(id) => println!("Tick({id})"),
+        }
     }
 }
 
 #[tokio::main]
 async fn main() {
-    tokio::spawn(ticker());
-    let counted_primes = slow_counter().await;
-    println!("{counted_primes}");
+    let (tx, rx) = tokio::sync::mpsc::channel::<Message>(100);
+    tokio::spawn(sender1(tx.clone()));
+    tokio::spawn(sender2(tx));
+    receiver(rx).await;
 }
